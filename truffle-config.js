3da4d969bfb9c80e3475db81acad50ea3575ce3d
Dhiraj <98685837+dhi-raj07@users.noreply.github.com>
2022-11-18 21:56:09 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -13,23 +13,22 @@ module.exports = {
         // Setting the provider with the Infura Mainnet address and Token
         return new HDWalletProvider(
           mnemonic,
-          "wss://rinkeby.infura.io/ws/v3/660b419a2cf84c9cbb18326146117d09"
+          "https://fabled-silent-silence.ethereum-goerli.discover.quiknode.pro/dd856c5890e38c4b47228a6b01a710ad4c1b1995/"
         );
       },
       network_id: "1",
     },
     // Configuration for rinkeby network
-    rinkeby: {
+    goerli: {
       // Special function to setup the provider
       provider: function () {
         // Setting the provider with the Infura Rinkeby address and Token
         return new HDWalletProvider(
           mnemonic,
-          "wss://rinkeby.infura.io/ws/v3/660b419a2cf84c9cbb18326146117d09"
+          "https://fabled-silent-silence.ethereum-goerli.discover.quiknode.pro/dd856c5890e38c4b47228a6b01a710ad4c1b1995/"
         );
       },
-      // Network id is 4 for Rinkeby
-      network_id: 4,
+      network_id: 5,
     },
     ganache: {
       host: "127.0.0.1",
